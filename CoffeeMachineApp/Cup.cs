720e48a2c542d8b060e24fb46caba8b442d9787a
Vladislav Skalozub <vladskalozub97@gmail.com>
2023-10-29 18:30:00 +0100

    Add project files.

---
+++ CoffeeMachineApp/Cup.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeMachineApp
+{
+    internal class Cup : CupBase, IContainingCoffee, IContainingWater, IContainingMilk, IContainingSugar
+    {
+        /// <summary>
+        /// Method from abstract class "CupBase" for mixing drinks
+        /// </summary>
+        public override void Stir()
+        {
+            Console.WriteLine("The drink mixed");
+        }
+
+        /// <summary>
+        /// Method, which adding coffee
+        /// </summary>
+        public void AddCoffee()
+        {
+            Console.WriteLine("Coffee was added");
+        }
+
+        /// <summary>
+        /// Method for adding water
+        /// </summary>
+        public void AddWater()
+        {
+            Console.WriteLine("Water was added");
+        }
+
+        /// <summary>
+        /// Method for adding milk
+        /// </summary>
+        public void AddMilk()
+        {
+            Console.WriteLine("Milk was added");
+        }
+
+        /// <summary>
+        /// Method for adding sugar
+        /// </summary>
+        public void AddSugar()
+        {
+            Console.WriteLine("Sugar was added");
+        }
+
+
+
+    }
+}
---
+++ CoffeeMachineApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeMachineApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cup cup = new Cup();
+
+            cup.AddCoffee();
+            cup.AddMilk();
+            cup.AddSugar();
+            cup.Stir();
+
+
+        }
+    }
+}
